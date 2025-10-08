2203965a696d1e2b1d54499423c137d22e51d4b2
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-08 22:11:48 +0200

    prepare 1.21.10 port

--- 1.21.9/Common/src/main/java/fuzs/permanentsponges/client/PermanentSpongesClient.java
+++
@@ -1,7 +0,0 @@
-package fuzs.permanentsponges.client;
-
-import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
-
-public class PermanentSpongesClient implements ClientModConstructor {
-
-}
